a82bfff6d15912d920db3a0c513347220b961b8e
Jaeho Yoo <jaeho.yoo@navercorp.com>
2022-10-08 18:35:11 +0900

    2장

--- books/Go 마스터하기/test.go
+++ books/Go 마스터하기/test.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
-	"log/syslog"
+	"os"
+	"strconv"
 )
 
 func main() {
-	sysLog, err := syslog.New(syslog.LOG_ALERT|syslog.LOG_MAIL, "Some program!")
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.SetOutput(sysLog)
+	if len(os.Args) == 1 {
+		fmt.Println("Please give one or more floats.")
+		os.Exit(1)
 	}
 
-	log.Panic(sysLog)
-	fmt.Println("Will you see this?")
+	arguments := os.Args
+	var err error = errors.New("An error")
+	k := 1
+	var n float64
+	for err != nil {
+		if k >= len(arguments) {
+			fmt.Println("None of the arguments is a float!")
+			return
+		}
+		n, err = strconv.ParseFloat(arguments[k], 64)
+		k++
+	}
+
+	min, max := n, n
+	for i := 2; i < len(arguments); i++ {
+		n, err := strconv.ParseFloat(arguments[i], 64)
+		if err == nil {
+			if n < min {
+				min = n
+			}
+			if n > max {
+				max = n
+			}
+		}
+	}
+
+	fmt.Println("Min:", min)
+	fmt.Println("Max:", max)
 }
